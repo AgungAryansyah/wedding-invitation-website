9244ed2314c074cd144d56c64e56488ef7573f3f
agungaryansyah <agungaryansyah2006@gmail.com>
2025-08-13 18:34:39 +0700

    feat: add db connection

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
 	"wedding-invitation-website/pkg/fiber"
+	"wedding-invitation-website/pkg/postgres"
 )
 
 func main() {
+	_, err := postgres.Connect()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Println("Connected to the database successfully")
 
 	app := fiber.Start()
-	app.Listen(":" + "3000")
+	app.Listen(":" + os.Getenv("APP_PORT"))
+
 }
