a848419570343bc751e8e5270335aef2c2db1b8e
agungaryansyah <agungaryansyah2006@gmail.com>
2025-08-21 10:50:58 +0700

    ref: move pagination metadata into a new file

---
+++ backend/model/dto/meta.go
@@ -0,0 +1,8 @@
+package dto
+
+type PaginationMeta struct {
+	Page       int   `json:"page"`
+	PageSize   int   `json:"page_size"`
+	Total      int64 `json:"total"`
+	TotalPages int   `json:"total_pages"`
+}
